20acd4a0e9fbdc72438e411c388fdae253bec9df
saikiran2001 <bjsaikiran@gmail.com>
2023-08-20 19:49:16 +0530

    nabu: Add OnePlus Dolby

--- device.mk
+++ device.mk
@@ -242,6 +242,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
